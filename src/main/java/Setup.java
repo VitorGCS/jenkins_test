20e2b8e321274c840ff130789778d8694273b750
vitor <vgc_silva@hotmail.com>
2022-08-20 12:32:27 +0100

    Webhook

--- src/main/java/Setup.java
+++ src/main/java/Setup.java
@@ -1,5 +1,6 @@
 public class Setup {
     public static void main(String [] args){
         System.out.println("Running Jenkins");
+        System.out.println("Let's trigger a build with a webhook !!!!");
     }
 }
